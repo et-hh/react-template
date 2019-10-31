f13cd477f5b86141680e167ea501839fca6785dd
gujun <gujun@yiwise.com>
2019-10-31 10:32:12 +0800

    语料库

--- src/pages/corpus/index.js
+++ src/pages/corpus/index.js
@@ -1,50 +1,107 @@
-import React from 'react'
-import { Button } from 'antd'
-import { BatchImportModal } from '@/components'
-import { CorpusModal } from './components'
+import React, { PureComponent } from 'react'
+import { Input, Select, Button, Checkbox } from 'antd'
+import { PaginationTable } from '@/components'
+import styles from './index.scss'
 
-export default class extends React.Component {
-  state = {
-    corpusModalVisible: false,
-    batchModalVisible: false,
-    corpusModalType: 'add'
-  }
+const { Option } = Select
+const { Search } = Input
+const CheckboxGroup = Checkbox.Group
 
-  toggleCorpusVisible(visible, type = 'add') {
-    this.setState({
-      corpusModalVisible: visible,
-      corpusModalType: type
-    })
-  }
-
-  toggleBatchImportVisible(visible) {
-    this.setState({
-      batchModalVisible: visible
-    })
-  }
-
-  handleModalSuccess = () => {
-    console.log('添加成功')
-  }
+export default class corpus extends PureComponent {
+  state = {}
 
   render() {
-    const { corpusModalVisible, batchModalVisible, corpusModalType } = this.state
+    const options = [
+      { label: '场景名称',value: 0},
+      { label: '意图名称', value: 1},
+      { label: '标准问题', value: 2},
+      { label: '相似问题', value: 3},
+      { label: '问题ID', value: 4}
+    ]
+
+    const tOptions = [
+      { label: '意图语料', value: 'Apple' },
+      { label: '知识库语料', value: 'Pear' },
+      { label: '会话日志语料', value: 'Orange' }]
+
+    const columns = [
+      {
+        title: '语料id',
+        dataIndex: 'name',
+        key: 'name',
+        width: 200
+      },
+      {
+        title: '意图名称/标准问题',
+        dataIndex: 'age',
+        key: 'age',
+        width: 300
+      },
+      {
+        title: '语料问法',
+        dataIndex: 'address',
+        key: 'address',
+        width: 200
+      },
+      {
+        title: '场景名称',
+        dataIndex: 'address1',
+        key: 'address1',
+        width: 200
+      },
+      {
+        title: '语料归类',
+        dataIndex: 'address2',
+        key: 'address2',
+        width: 200
+      },
+      {
+        title: '操作',
+        dataIndex: 'address3',
+        key: 'address3',
+        render: () => (
+          <span>
+            <Button type="link">编辑</Button>
+            <Button type="link">删除</Button>
+            <Button type="link">添加到导出列表</Button>
+          </span>
+        )
+      }
+    ]
+
     return (
-      <div>
-        <Button onClick={() => this.toggleCorpusVisible(true, 'add')}>新建语料</Button>
-        <Button onClick={() => this.toggleCorpusVisible(true, 'edit')}>编辑语料</Button>
-        <Button onClick={() => this.toggleBatchImportVisible(true)}>批量导入</Button>
-        <CorpusModal
-          visible={corpusModalVisible}
-          type={corpusModalType}
-          onToggleVisible={visible => this.toggleCorpusVisible(visible)}
+      <div className={styles.panel}>
+        <Select defaultValue={options[0].value} style={{ width: 120 }}>
+          { options.map( item => (<Option key={item.value} value={item.value}>{item.label}</Option>)) }
+        </Select>
+        <Search
+          placeholder="查询问题"
+          className={styles.search}
+          onSearch={value => console.log(value)}
+          style={{ width: 280 }}
         />
-        <BatchImportModal
-          visible={batchModalVisible}
-          onToggleVisible={visible => this.toggleBatchImportVisible(visible)}
-        >
-        </BatchImportModal>
+        <Button type="primary" style={{marginLeft: '16px'}}>新建语料</Button>
+        <div>
+          <Checkbox>
+            全部
+          </Checkbox>
+          <CheckboxGroup
+            options={tOptions}
+          />
+          <Button style={{marginLeft: '16px'}}>添加到导出列表</Button>
+          <Button style={{marginLeft: '16px'}}>导入</Button>
+          <Button style={{marginLeft: '16px'}}>导出</Button>
+          <Button style={{marginLeft: '16px'}}>删除</Button>
+        </div>
+        <PaginationTable
+          selection={true}
+          columns={columns}/>
       </div>
     )
   }
 }
+
+
+
+
+
